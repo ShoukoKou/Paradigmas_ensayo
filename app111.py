c6a782c5f0122f1310e7f6170bdd2e73708b7c8b
Kouu <honnegiohe@gmail.com>
2025-09-12 09:42:16 -0500

    app111.py

--- app111.py
+++ app111.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.tittle("Gatitos")
-num = st.slider("num", 0, 100, step = 1)
+st.title("Gatitos")
+num=st.slider("num", 0, 100, step = 1)
 st.write("El número ingresado es {}".format(num))
