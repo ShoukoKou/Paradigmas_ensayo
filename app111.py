0a1a0359203ce2b8f2b01af33e368c0355b83225
Kouu <honnegiohe@gmail.com>
2025-09-12 09:56:37 -0500

    app111.py

--- app111.py
+++ app111.py
@@ -1,7 +1,13 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 
-st.title("Gatitos")
-num=st.slider("num", 0, 100, step = 1)
-st.write("El número ingresado es {}".format(num))
+st.title("Gatitos - Edades")
+
+edades = st.text_input("Ingresa edades de gatos separadas por comas:", "2,4,7")
+
+if edades:
+    lista = [int(x) for x in edades.split(",") if x.strip().isdigit()]
+    if lista:
+        st.write("Edad mínima:", min(lista))
+        st.write("Edad máxima:", max(lista))
+        st.write("Edad promedio:", round(np.mean(lista),2))
